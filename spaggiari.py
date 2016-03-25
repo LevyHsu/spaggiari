ae05f82e01f54acfc877a0d251dadf8deaae3f54
Dionysus <acidvegas@users.noreply.github.com>
2016-03-25 15:54:43 -0400

    Update spaggiari.py

--- spaggiari.py
+++ spaggiari.py
@@ -35,7 +35,7 @@ Todo:
 - Implement scanning for telnet and ftp.
 - Make use of 'deep_combos' after no successful logins.
 
-Legal Disclaimer
+Legal Disclaimer:
 It is the end user's responsibility to obey all applicable local, state and federal laws.
 Developers assume no liability and are not responsible for any misuse or damage caused by this program.
 """
